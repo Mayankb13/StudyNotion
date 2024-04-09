0a86042beee66c4c6ed5d197d0fa1b332033f520
mayank <himanshushaurya13@gmail.com>
2024-04-09 17:46:36 +0530

    First Commit StudyNotion

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom'
 import { apiConnector } from '../services/apiconnector';
 import { categories } from '../services/apis';
 import { getCatalogPageData } from '../services/operations/pageAndComponentData';
-import Course_Card from '../components/core/Catalog/Course_Card';
+import Coursecard from '../components/core/Catalog/Coursecard';
 import CourseSlider from '../components/core/Catalog/CourseSlider';
 import { useSelector } from "react-redux"
 import Error from "./Error"
@@ -126,7 +126,7 @@ const Catalog = () => {
                         {catalogPageData?.data?.mostSellingCourses
                             ?.slice(0, 4)
                             .map((course, i) => (
-                                <Course_Card course={course} key={i} Height={"h-[400px]"} />
+                                <Coursecard course={course} key={i} Height={"h-[400px]"} />
                             ))}
                     </div>
                 </div>
