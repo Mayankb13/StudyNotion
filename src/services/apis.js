144154c835165fbdffa28f86457d00bb9f9f8454
mayank <himanshushaurya13@gmail.com>
2024-04-15 17:13:49 +0530

    changed backend route in api.js

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,6 +1,6 @@
 // require('dotenv').config()
 // import 'dotenv/config'
-const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = "https://studynotion-backend-mr7g.onrender.com/api/v1"
 
 // AUTH ENDPOINTS
 export const endpoints = {
