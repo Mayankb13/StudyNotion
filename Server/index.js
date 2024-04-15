26072788db714d5eea7d8a61321a461347f1bc8a
mayank <himanshushaurya13@gmail.com>
2024-04-15 17:18:21 +0530

    removes cors error

--- Server/index.js
+++ Server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://study-notion-frontend-4fq90dbu6-mayankb13s-projects.vercel.app/",
 		credentials:true,
 	})
 )
