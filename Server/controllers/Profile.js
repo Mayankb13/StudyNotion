80043be4d7149bdd9010ef4a1659723a54cabdcc
mayank <himanshushaurya13@gmail.com>
2024-04-15 16:52:57 +0530

    removed some errors

--- Server/controllers/Profile.js
+++ Server/controllers/Profile.js
@@ -146,7 +146,7 @@ exports.updateDisplayPicture = async (req, res) => {
 
 exports.getEnrolledCourses = async (req, res) => {
   try {
-    const userId = req.user.id
+    const userId = req.user.id;
     let userDetails = await User.findOne({
       _id: userId,
     })
@@ -160,12 +160,12 @@ exports.getEnrolledCourses = async (req, res) => {
         },
       })
       .exec()
-    userDetails = userDetails.toObject()
+      userDetails = userDetails.toObject()
     var SubsectionLength = 0
-    for (var i = 0; i < userDetails.courses.length; i++) {
+    for (var i = 0; i < userDetails.courses?.length; i++) {
       let totalDurationInSeconds = 0
       SubsectionLength = 0
-      for (var j = 0; j < userDetails.courses[i].courseContent.length; j++) {
+      for (var j = 0; j < userDetails.courses[i].courseContent?.length; j++) {
         totalDurationInSeconds += userDetails.courses[i].courseContent[
           j
         ].subSection.reduce((acc, curr) => acc + parseInt(curr.timeDuration), 0)
@@ -179,7 +179,7 @@ exports.getEnrolledCourses = async (req, res) => {
         courseID: userDetails.courses[i]._id,
         userId: userId,
       })
-      courseProgressCount = courseProgressCount?.completedVideo.length
+      courseProgressCount = courseProgressCount?.completedVideos?.length
       if (SubsectionLength === 0) {
         userDetails.courses[i].progressPercentage = 100
       } else {
--- Server/controllers/courseProgress.js
+++ Server/controllers/courseProgress.js
@@ -7,20 +7,22 @@ const Course = require("../models/Course")
 exports.updateCourseProgress = async (req, res) => {
   const { courseId, subsectionId } = req.body
   const userId = req.user.id
-
+  console.log('reached here' , courseId , subsectionId);
   try {
     // Check if the subsection is valid
     const subsection = await SubSection.findById(subsectionId)
     if (!subsection) {
       return res.status(404).json({ error: "Invalid subsection" })
     }
-
+    console.log('1');
+    
     // Find the course progress document for the user and course
     let courseProgress = await CourseProgress.findOne({
       courseID: courseId,
       userId: userId,
     })
-
+    console.log('1');
+    
     if (!courseProgress) {
       // If course progress doesn't exist, create a new one
       return res.status(404).json({
@@ -29,16 +31,19 @@ exports.updateCourseProgress = async (req, res) => {
       })
     } else {
       // If course progress exists, check if the subsection is already completed
+      console.log('1');
       if (courseProgress.completedVideos.includes(subsectionId)) {
         return res.status(400).json({ error: "Subsection already completed" })
       }
-
+      
+      console.log('1');
       // Push the subsection into the completedVideos array
       courseProgress.completedVideos.push(subsectionId)
     }
-
+    
     // Save the updated course progress
     await courseProgress.save()
+    console.log('1');
 
     return res.status(200).json({ message: "Course progress updated" })
   } catch (error) {
