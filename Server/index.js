60eda245a64996893cdb86846eb6d2daa3104117
mayank <himanshushaurya13@gmail.com>
2024-04-15 17:34:58 +0530

    add origin

--- Server/index.js
+++ Server/index.js
@@ -23,8 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://study-notion-frontend-4fq90dbu6-mayankb13s-projects.vercel.app",
-		origin:"https://study-notion-frontend-two-chi.vercel.app/",
+		origin:["https://study-notion-frontend-4fq90dbu6-mayankb13s-projects.vercel.app","study-notion-frontend-two-chi.vercel.app"],
 		credentials:true,
 	})
 )
